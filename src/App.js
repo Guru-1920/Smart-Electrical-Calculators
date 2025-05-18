bec18dcda42cb4e177e46c4e701ed0ca934a159c
Guru-1920 <infiniteskyltd2@gmail.com>
2025-05-18 14:08:16 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import LoadCalculator from "./Components/LoadCalculator";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 p-4">
-      <LoadCalculator />
-    </div>
+    <main className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
+      <div className="w-full max-w-4xl bg-white shadow-xl rounded-2xl p-6">
+        <LoadCalculator />
+      </div>
+    </main>
   );
 }
 
